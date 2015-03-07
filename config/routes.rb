67cfda287dc19a6bfdb1f35b1634530698a973cd
aloktayi <alok.s.tayi@gmail.com>
2015-03-07 13:03:47 -0500

    message

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'static_pages#main_page'
+  root 'tweets#new'
+
+  resources :tweets
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
